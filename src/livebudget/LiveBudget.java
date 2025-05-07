d1a29b37067af62aa40b0766d14fcf835851dde3
Khatama <152944169+moenalyze@users.noreply.github.com>
2025-05-07 13:26:26 +0700

    First commit

---
+++ src/livebudget/LiveBudget.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package livebudget;
+
+/**
+ *
+ * @author Khatama
+ */
+public class LiveBudget {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
