94b35ec9e838a68493a9506ed8b9896d15d9de68
Khatama <152944169+moenalyze@users.noreply.github.com>
2025-05-08 08:11:39 +0700

    Menambahkan package

--- src/livebudget/LiveBudget.java
+++ src/livebudget/LiveBudget.java
@@ -4,6 +4,8 @@
  */
 package livebudget;
 
+import views.LoginRegister;
+
 /**
  *
  * @author Khatama
@@ -15,6 +17,8 @@ public class LiveBudget {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LoginRegister login = new LoginRegister();
+        login.setVisible(true);
     }
     
 }
